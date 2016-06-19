531ec9da99f7a6f992f4017b536a8d3c919bfe7e
Zezombye <magmacubenetherrique@gmail.com>
2016-06-19 13:27:48 +0200

    Max character limit is now 200

--- fonts.h
+++ fonts.h
@@ -2,8 +2,8 @@
 #define FONTS_H
 
 struct Font {
-	unsigned long ltr[130][2]; //an array of longs for each character
-	char length[130]; //array of chars for the length of each character
+	unsigned long ltr[200][2]; //an array of longs for each character
+	char length[200]; //array of chars for the length of each character
 };
 
 extern struct Font normfont;
