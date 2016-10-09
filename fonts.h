9104c760b916cd665ea6cd69caffc3ec3a76b5cd
Zezombye <magmacubenetherrique@gmail.com>
2016-10-09 15:45:10 +0200

    Update fonts.h

--- fonts.h
+++ fonts.h
@@ -2,12 +2,12 @@
 #define FONTS_H
 
 struct Font {
-	unsigned long ltr[200][2]; //an array of longs for each character
-	char length[200]; //array of chars for the length of each character
+	unsigned long ltr[220][2]; //an array of longs for each character
+	char length[220]; //array of chars for the length of each character
 };
 
 extern struct Font normfont;
 
-int dispStr(unsigned char* str, struct Font font, int x, int y, int strlen);
+int dispStr(unsigned char* str, int x, int y);
 
 #endif
